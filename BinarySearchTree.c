940f9e97e2874ab0aa3f67d6e50e6cfac4ebdcfe
Habib Sabiu <habibado2006@gmail.com>
2018-06-28 15:55:14 -0600

    Implemented function to search for nodes in a binary search tree

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 
 
 struct BSTNode{
@@ -8,24 +9,60 @@ struct BSTNode{
     struct BSTNode* rightChild;
 };
 
-struct BSTNode* insertNode(struct BSTNode*, int);
 struct BSTNode* createNewNode(int);
+struct BSTNode* insertNode(struct BSTNode*, int);
+bool searchNode(struct BSTNode*, int);
 
 int main(void){
 
     struct BSTNode* rootNode = NULL;
     //struct BSTNode* pointerAtIndex = NULL;
 
+    puts("\n");
+
+    printf("Creating a Binary Search Tree...\n");
+
+    puts("\n");
+
+    printf("Inserting 10 into the BST...\n");
     rootNode = insertNode(rootNode, 10);
+
+    puts("\n");
+
+    printf("Inserting 5 into the BST...\n");
     rootNode = insertNode(rootNode, 5);
+
+    puts("\n");
+
+    printf("Inserting 20 into the BST...\n");
     rootNode = insertNode(rootNode, 20);
+
+    puts("\n");
+
+    printf("Inserting 8 into the BST...\n");
     rootNode = insertNode(rootNode, 8);
+
+    puts("\n");
+
+    printf("Inserting 15 into the BST...\n");
     rootNode = insertNode(rootNode, 15);
+
+    puts("\n");
+
+    printf("Searching for node with value 10 in the BST... Search should return 1... \n");
+    printf("===> %d \n", searchNode(rootNode, 20));
+
+    puts("\n");
+
+    printf("Searching for node with value 50 in the BST... Search should return 0... \n");
+    printf("===> %d \n", searchNode(rootNode, 50));
+
+    puts("\n");
 
     puts("------------------------------");
     puts("SUCCESS: ALL TEST CASES PASSED");
     puts("------------------------------");
-    
+
     return 0;
 }
 
@@ -51,4 +88,12 @@ struct BSTNode* insertNode(struct BSTNode* rootNode, int item){
     }
 
     return rootNode;
+}
+
+bool searchNode(struct BSTNode* rootNode, int item){
+
+    if (rootNode == NULL) return false;
+    else if (rootNode->data == item) return true;
+    else if (item <= rootNode->data) return searchNode(rootNode->leftChild, item);
+    else return searchNode(rootNode->rightChild, item);
 }
