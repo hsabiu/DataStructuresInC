1f677a0548dc796793482441048475d4f8c124ee
Habib Sabiu <habibado2006@gmail.com>
2018-06-28 15:35:36 -0600

    Implemented functions to create and insert nodes in a binary search tree

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -2,13 +2,53 @@
 #include<stdlib.h>
 
 
-struct Node{
+struct BSTNode{
     int data;
-    struct Node* leftChild;
-    struct Node* rightChild;
+    struct BSTNode* leftChild;
+    struct BSTNode* rightChild;
 };
+
+struct BSTNode* insertNode(struct BSTNode*, int);
+struct BSTNode* createNewNode(int);
 
 int main(void){
 
+    struct BSTNode* rootNode = NULL;
+    //struct BSTNode* pointerAtIndex = NULL;
+
+    rootNode = insertNode(rootNode, 10);
+    rootNode = insertNode(rootNode, 5);
+    rootNode = insertNode(rootNode, 20);
+    rootNode = insertNode(rootNode, 8);
+    rootNode = insertNode(rootNode, 15);
+
+    puts("------------------------------");
+    puts("SUCCESS: ALL TEST CASES PASSED");
+    puts("------------------------------");
+    
     return 0;
+}
+
+struct BSTNode* createNewNode(int item){
+    struct BSTNode* newNode = (struct BSTNode*) malloc(sizeof(struct BSTNode));
+    newNode->data = item;
+    newNode->leftChild = NULL;
+    newNode->rightChild = NULL;
+    return newNode;
+}
+
+struct BSTNode* insertNode(struct BSTNode* rootNode, int item){
+
+    if (rootNode == NULL){
+        struct BSTNode* newNode = createNewNode(item);
+        rootNode = newNode;
+    }
+    else if(item <= rootNode->data){
+        rootNode->leftChild = insertNode(rootNode->leftChild, item);
+    }
+    else{
+        rootNode->rightChild = insertNode(rootNode->rightChild, item);
+    }
+
+    return rootNode;
 }
